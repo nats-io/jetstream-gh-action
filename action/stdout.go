fff4d06ce5deba9b84db8e1cf528e39a1db1e670
R.I.Pienaar <rip@devco.net>
2020-05-15 17:06:00 +0200

    add additional logging

---
+++ action/stdout.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type stdout struct{}
+
+func (s stdout) GetInput(k string) string {
+	return os.Getenv(strings.ToUpper(k))
+}
+
+func (s stdout) SetOutput(k string, v string) error {
+	return ioutil.WriteFile(filepath.Join("/worksspace/output", k, "output.txt"), []byte(v), 0644)
+}
+
+func (s stdout) Fatalf(msg string, args ...interface{}) {
+	log.Fatalf(msg, args...)
+}
+
+func (s stdout) Debugf(msg string, args ...interface{}) {
+	log.Printf(msg, args...)
+}
+
+func (s stdout) Printf(msg string, args ...interface{}) {
+	log.Printf(msg, args...)
+}
